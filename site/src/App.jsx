ac6e83d6e6e686c6cedcacd5b8154eea4ad565a0
YoshioSM <88592991+YoshioSM@users.noreply.github.com>
2025-08-01 14:21:28 -0600

    update

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import './index.css'
+import { useState } from "react";
+import Navbar from "./components/navbar";
+import SplitTextTitle from "./components/navbar/SplitText";
+import "./index.css";
 
 function App() {
-
-
   return (
-    <>
-      <h1 className='text-center'>hola mundo</h1>
-    </>
-  )
+    <div>
+      <Navbar />
+
+      <SplitTextTitle
+        text="Hello, GSAP!"
+        className="text-2xl font-semibold text-center"
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
