ecf5b94ca7c701be6e4cb54c6a67a9a68590dafd
YoshioSM <88592991+YoshioSM@users.noreply.github.com>
2025-08-07 16:09:33 -0600

    update

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/navbar";
 import SplitTextTitle from "./components/SplitText/index";
 import VideoBackground from "./components/videoBackground";
 import videoSrc from "./assets/pacifico37.mp4";
+import Stack from "./components/Stack";
 import "./index.css";
 
 function App() {
@@ -10,20 +11,50 @@ function App() {
     <div div class="@container">
       <Navbar />
       <VideoBackground videoSrc={videoSrc}>
-        <SplitTextTitle
-          text="Tu nueva casa te espera!"
-          className="text-2xl font-semibold text-center m-2"
-          delay={60}
-          duration={2}
-          ease="bounce.out"
-          splitType="chars"
-          from={{ opacity: 0, y: 40 }}
-          to={{ opacity: 1, y: 0 }}
-          threshold={0.1}
-          rootMargin="-100px"
-          textAlign="center"
-        />
+        <div>
+          <SplitTextTitle
+            text="Tu nueva casa te espera!"
+            className="text-2xl font-semibold text-center m-2"
+            delay={10}
+            duration={2}
+            ease="power3.out"
+            splitType="chars"
+            from={{ opacity: 0, y: 40 }}
+            to={{ opacity: 1, y: 0 }}
+            threshold={0.1}
+            rootMargin="-100px"
+            textAlign="center"
+          />
+        </div>
+        <div>
+          <p>conoce este marivillos lugar</p>
+          <button className="flex items-center gap-2 px-4 py-2 text-white bg-black rounded-lg duration-150 hover:bg-white ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M6.32 2.577a49.255 49.255 0 0111.36 0c1.497.174 2.57 1.46 2.57 2.93V21a.75.75 0 01-1.085.67L12 18.089l-7.165 3.583A.75.75 0 013.75 21V5.507c0-1.47 1.073-2.756 2.57-2.93z"
+                clipRule="evenodd"
+              />
+            </svg>
+            Button
+          </button>
+        </div>
       </VideoBackground>
+      <div>
+        <div>
+          <h1>hola</h1>
+        </div>
+        <div>
+          <Stack 
+          
+          />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ site/src/components/Stack/index.jsx
@@ -0,0 +1,120 @@
+import { motion, useMotionValue, useTransform } from "framer-motion";
+import foto1 from "../../assets/Fotos/1.jpg";
+import foto2 from "../../assets/Fotos/4.jpg";
+import foto3 from "../../assets/Fotos/5.jpg";
+import foto4 from "../../assets/Fotos/6.jpg";
+import { useState } from "react";
+
+function CardRotate({ children, onSendToBack, sensitivity }) {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const rotateX = useTransform(y, [-100, 100], [60, -60]);
+  const rotateY = useTransform(x, [-100, 100], [-60, 60]);
+
+  function handleDragEnd(_, info) {
+    if (
+      Math.abs(info.offset.x) > sensitivity ||
+      Math.abs(info.offset.y) > sensitivity
+    ) {
+      onSendToBack();
+    } else {
+      x.set(0);
+      y.set(0);
+    }
+  }
+
+  return (
+    <motion.div
+      className="absolute cursor-grab"
+      style={{ x, y, rotateX, rotateY }}
+      drag
+      dragConstraints={{ top: 0, right: 0, bottom: 0, left: 0 }}
+      dragElastic={0.6}
+      whileTap={{ cursor: "grabbing" }}
+      onDragEnd={handleDragEnd}
+    >
+      {children}
+    </motion.div>
+  );
+}
+
+export default function Stack({
+  randomRotation = false,
+  sensitivity = 100,
+  cardDimensions = { width: 308, height: 308 },
+  cardsData = [],
+  animationConfig = { stiffness: 260, damping: 20 },
+  sendToBackOnClick = true
+}) {
+  const [cards, setCards] = useState(
+    cardsData.length
+      ? cardsData
+      : [
+        { id: 1, img: foto1 },
+        { id: 2, img: foto2 },
+        { id: 3, img: foto3 },
+        { id: 4, img: foto4 }
+      ]
+  );
+
+  const sendToBack = (id) => {
+    setCards((prev) => {
+      const newCards = [...prev];
+      const index = newCards.findIndex((card) => card.id === id);
+      const [card] = newCards.splice(index, 1);
+      newCards.unshift(card);
+      return newCards;
+    });
+  };
+
+  return (
+    <div
+      className="relative"
+      style={{
+        width: cardDimensions.width,
+        height: cardDimensions.height,
+        perspective: 600,
+      }}
+    >
+      {cards.map((card, index) => {
+        const randomRotate = randomRotation
+          ? Math.random() * 10 - 5
+          : 0;
+
+        return (
+          <CardRotate
+            key={card.id}
+            onSendToBack={() => sendToBack(card.id)}
+            sensitivity={sensitivity}
+          >
+            <motion.div
+              className="rounded-2xl overflow-hidden border-4 border-slate-600"
+              onClick={() => sendToBackOnClick && sendToBack(card.id)}
+              animate={{
+                rotateZ: (cards.length - index - 1) * 4 + randomRotate,
+                scale: 1 + index * 0.06 - cards.length * 0.06,
+                transformOrigin: "90% 90%",
+              }}
+              initial={false}
+              transition={{
+                type: "spring",
+                stiffness: animationConfig.stiffness,
+                damping: animationConfig.damping,
+              }}
+              style={{
+                width: cardDimensions.width,
+                height: cardDimensions.height,
+              }}
+            >
+              <img
+                src={card.img}
+                alt={`card-${card.id}`}
+                className="w-full h-full object-cover pointer-events-none"
+              />
+            </motion.div>
+          </CardRotate>
+        );
+      })}
+    </div>
+  );
+}
